69cce037c6e564e1e3cb33a87b15f8f4cd9df0f2
Arslaan Siddiqui <arslaansiddiqui23@gmail.com>
2025-03-27 15:49:37 +0530

    add query to delete a user in query.py

--- src/query.py
+++ src/query.py
@@ -7,10 +7,9 @@ DB_PATH = os.path.abspath(r"E:\Face-Recognition-for-Login-Authentication-System\
 conn = sqlite3.connect(DB_PATH)
 cursor = conn.cursor()
 
-# cursor.execute('SELECT name, face_encoding FROM users')
-# rows = cursor.fetchall()
+# cursor.execute("DELETE FROM users WHERE name = 'Arslaan Siddiqui'")
 cursor.execute("DELETE FROM users")
-cursor.execute("DELETE FROM sqlite_sequence")
+# cursor.execute("DELETE FROM sqlite_sequence")
 
 conn.commit()
 conn.close()
