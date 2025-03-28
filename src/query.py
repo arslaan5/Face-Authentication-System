202e9a7aa5891c9a222cfb0c90e4b6d01ee3b2cd
Arslaan Siddiqui <arslaansiddiqui23@gmail.com>
2025-03-28 20:44:35 +0530

    refactor database connection to use environment variable for DB_PATH

--- src/query.py
+++ src/query.py
@@ -1,15 +1,17 @@
 import sqlite3
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-DB_PATH = os.path.abspath(r"E:\Face-Recognition-for-Login-Authentication-System\face_recognition.db")
+DB_PATH = os.getenv("DB_PATH")
 
 conn = sqlite3.connect(DB_PATH)
 cursor = conn.cursor()
 
 # cursor.execute("DELETE FROM users WHERE name = 'Arslaan Siddiqui'")
 cursor.execute("DELETE FROM users")
-# cursor.execute("DELETE FROM sqlite_sequence")
+cursor.execute("DELETE FROM sqlite_sequence")
 
 conn.commit()
 conn.close()
